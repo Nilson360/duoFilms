ed34b67b0ff43d179fa497743f4398ac26bb2e69
Nilson360 <nilsonsimao360@gmail.com>
2024-12-10 20:03:23 +0100

    ajout de nouvelles fonctionnalités

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $message = new Message();
+        $form = $this->createForm(ContactType::class, $message);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'A sua mensagem foi enviada com sucesso!');
+
+            return $this->redirectToRoute('app_home');
+        }
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'form' => $form,
         ]);
     }
 
@@ -20,6 +37,27 @@ class HomeController extends AbstractController
     public function poortofolio(): Response
     {
         return $this->render('portofolio/index.html.twig', [
+        ]);
+    }
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $message = new Message();
+        $form = $this->createForm(ContactType::class, $message);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Your message has been sent successfully!');
+
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
